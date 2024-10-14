e66bb6a05ea13f2098158fa12e4a60ea7c71689d
maleha <maleha-7@internet.ru>
2024-10-14 20:05:21 +0400

    Task 3 done.

---
+++ Prac8/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Prac8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task3();
+        }
+
+        static void Task3()
+        {
+            /* 
+             * III. Дана строка, в которой содержится осмысленное текстовое сообщение. Слова
+             * сообщения разделяются пробелами и знаками препинания.
+             * 13. Найти самое короткое слово сообщения.
+             */
+            string str = "Златая цепь на дубе том: И днём и ночью кот учёный Всё ходит по цепи кругом; Идёт направо - песнь заводит, Налево - сказку говорит.";
+            Console.WriteLine(str);
+            char[] div = { ' ', ':', ';', '-', ',', '.' };
+            String[] strSp = str.Split(div, StringSplitOptions.RemoveEmptyEntries);
+            int strMinLen = strSp[0].Length;
+            int strMinIndex = 0;
+            for (int i = 1; i < strSp.Length; i++)
+            {
+                if (strSp[i].Length < strMinLen)
+                {
+                    strMinLen = strSp[i].Length;
+                    strMinIndex = i;
+                }
+            }
+            Console.WriteLine(strSp[strMinIndex]);
+        }
+    }
+}
