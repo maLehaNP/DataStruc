444909739b8d6d475e3949fc972ab0337477f589
maleha <maleha-7@internet.ru>
2024-11-19 13:02:19 +0400

    2

--- Prac15/Program.cs
+++ Prac15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Threading;
 
 namespace Prac15
 {
@@ -16,22 +17,56 @@ namespace Prac15
             /* 13. Вывести на экран в порядке возрастания все элементы, меньшие заданного числа,
              * увеличив их в три раза.
              */
-            ArrayList array = Input();
+            var array = Read().ToArray();
             Console.Write("Введите число: ");
             int digit = int.Parse(Console.In.ReadLine());
+            var newArray =
+                from n in array
+                where n < digit
+                orderby n
+                select n * 3;
         }
 
         /// <summary>
         /// Создает массив из объектов в строках файла и возвращает ссылку на него.
         /// </summary>
-        static public ArrayList Input()
+        static public ArrayList Read()
         {
             using (StreamReader fileIn = new StreamReader(@"input1.txt"))
             {
                 ArrayList array = new ArrayList();
                 string line;
-                while ((line = fileIn.ReadLine()) != null) array.Add(line);
+                while ((line = fileIn.ReadLine()) != null) array.Add(int.Parse(line));
                 return array;
+            }
+        }
+
+        /// <summary>
+        /// Создает массив из объектов в строках файла и возвращает ссылку на него.
+        /// </summary>
+        static public int[] ReadInts()
+        {
+            using (StreamReader fileIn = new StreamReader(@"input1.txt"))
+            {
+                int n = int.Parse(fileIn.ReadLine());
+                int[] array = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = int.Parse(fileIn.ReadLine()));
+                }
+                return array;
+            }
+        }
+
+        static public void Write(ArrayList array)
+        {
+            using (StreamWriter fileOut = new StreamWriter(@"output1.txt"))
+            {
+                foreach (int elem in array)
+                {
+                    Console.WriteLine(elem);
+                    fileOut.WriteLine(elem);
+                }
             }
         }
     }
