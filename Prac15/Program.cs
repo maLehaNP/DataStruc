903d9c54929b30875784d51d2274c3c091ec8c94
maleha <maleha-7@internet.ru>
2024-11-19 12:30:08 +0400

    init

---
+++ Prac15/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Prac15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            //Task2();
+        }
+
+        public void Task1()
+        {
+            /* 13. Вывести на экран в порядке возрастания все элементы, меньшие заданного числа,
+             * увеличив их в три раза.
+             */
+
+        }
+
+        public int[]
+    }
+}
