529f92065b13ddfe90f65396cb5a893744382aee
maleha <maleha-7@internet.ru>
2024-11-26 00:19:00 +0400

    Final

--- Prac16/Program.cs
+++ Prac16/Program.cs
@@ -2,6 +2,32 @@
 
 namespace Prac16
 {
+    struct Toy : IComparable<Toy>
+    {
+        public string Name { get; }
+        public int Price { get; }
+        public int LowerAgeLimit { get; }
+        public int UpperAgeLimit { get; }
+
+        public Toy(string name, int price, int lowerAgeLimit, int upperAgeLimit)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.LowerAgeLimit = lowerAgeLimit;
+            this.UpperAgeLimit = upperAgeLimit;
+        }
+
+        public int CompareTo(Toy other)
+        {
+            return Price.CompareTo(other.Price);
+        }
+
+        public override string ToString()
+        {
+            return Name + ";" + Price + ";" + LowerAgeLimit + ";" + UpperAgeLimit;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -13,17 +39,17 @@ namespace Prac16
 
         public static void Task1()
         {
-            /* 13. Вывести на экран в порядке возрастания все элементы, меньшие заданного числа,
-             * увеличив их в три раза.
+            /* 14. Вывести на экран в порядке убывания все элементы, большие заданного числа, уменьшив
+             * их в три раза.
              */
             int[] ints = ReadInts();
 
             Console.Write("Введите число: ");
             int digit = int.Parse(Console.In.ReadLine());
 
-            var newInts = ints.Where(n => n < digit).OrderBy(n => n).Select(n => n * 3);
+            var newInts = ints.Where(n => n > digit).OrderDescending().Select(n => n / 3);
 
-            using (StreamWriter fileOut = new StreamWriter(@"output.txt"))
+            using (StreamWriter fileOut = new StreamWriter(@"output1.txt"))
             {
                 foreach (int elem in newInts)
                 {
@@ -38,7 +64,7 @@ namespace Prac16
         /// </summary>
         static public int[] ReadInts()
         {
-            using (StreamReader fileIn = new StreamReader(@"input.txt"))
+            using (StreamReader fileIn = new StreamReader(@"input1.txt"))
             {
                 int n = int.Parse(fileIn.ReadLine());
                 int[] array = new int[n];
@@ -52,6 +78,52 @@ namespace Prac16
 
         public static void Task2()
         {
+            /* Задание 14. На основе данных входного файла составить инвентарную ведомость
+             * игрушек, включив следующие данные: название игрушки, ее стоимость (в руб.), возрастные
+             * границы детей, для которых предназначена игрушка. Вывести в новый файл информацию о
+             * тех игрушках, которые предназначены для детей от N до M лет, сгруппировав их по
+             * названию.
+             */
+            Toy[] toys = ReadToys();
+            Console.Write("Введите N: ");
+            int N = int.Parse(Console.In.ReadLine());
+            Console.Write("Введите M: ");
+            int M = int.Parse(Console.In.ReadLine());
+
+            var newToys = toys.Where(toy => toy.LowerAgeLimit == N && toy.UpperAgeLimit == M).GroupBy(toy => toy.Name);
+
+            using (StreamWriter fileOut = new StreamWriter(@"output2.txt"))
+            {
+                foreach (var group in newToys)
+                {
+                    Console.Write("Группа {0}: ", group.Key);
+                    fileOut.Write("Группа " + group.Key + ": ");
+                    foreach (var toy in group)
+                    {   
+                        Console.Write("{0} ", toy.ToString());
+                        fileOut.Write(toy.ToString() + " ");
+                    }
+                    Console.WriteLine();
+                    fileOut.WriteLine();
+                }
+                Console.WriteLine();
+                fileOut.WriteLine();
+            }
+        }
+
+        static public Toy[] ReadToys()
+        {
+            using (StreamReader fileIn = new StreamReader(@"input2.txt"))
+            {
+                int num = int.Parse(fileIn.ReadLine());
+                Toy[] array = new Toy[num];
+                for (int i = 0; i < num; i++)
+                {
+                    string[] obj = fileIn.ReadLine().Split(';');
+                    array[i] = new Toy(obj[0], int.Parse(obj[1]), int.Parse(obj[2]), int.Parse(obj[3]));
+                }
+                return array;
+            }
         }
     }
 }
