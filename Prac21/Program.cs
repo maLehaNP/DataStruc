bae33ba187aab70a156247ec779bd93aa17b2b62
maleha <maleha@DESKTOP-TV89Q3E>
2025-03-13 23:27:54 +0400

    init

---
+++ Prac21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+// Практикум №21
+namespace Prac21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* I. В файле input.txt хранится последовательность целых чисел. По входной
+             * последовательности построить дерево бинарного поиска и найти для него:
+             * 13. сумму значений узлов в дереве, имеющих только одно правое поддерево.
+             */
+
+            BinaryTree tree = new BinaryTree();
+
+            // Считываем данные из файла в список
+            using (StreamReader fileIn = new StreamReader(@"input.txt"))
+            {
+                string line = fileIn.ReadToEnd();
+                char[] sep = { ' ', '\n', '\t', '\r' };
+                string[] data = line.Split(sep);
+                foreach (string item in data)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        tree.Add(int.Parse(item));
+                    }
+                }
+            }
+
+
+        }
+    }
+}
