b7a385a7ac6dd8736fbc9b15e4513df5097dc8df
maleha <maleha@DESKTOP-TV89Q3E>
2025-05-23 10:58:00 +0400

    Copy Graph

---
+++ Prac22/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Prac22
+{
+    class Program
+    {
+        /*public static BinaryTree ReadTree(string filename)
+        {
+            BinaryTree tree = new BinaryTree();
+            // Считываем данные из файла в список
+            using (StreamReader fileIn = new StreamReader(@filename))
+            {
+                string line = fileIn.ReadToEnd();
+                char[] sep = { ' ', '\n', '\t', '\r' };
+                string[] data = line.Split(sep);
+                foreach (string item in data)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        tree.Add(int.Parse(item));
+                    }
+                }
+            }
+            return tree;
+        }
+
+        public static AVLTree ReadAVLTree(string filename)
+        {
+            AVLTree tree = new AVLTree();
+            // Считываем данные из файла в список
+            using (StreamReader fileIn = new StreamReader(@filename))
+            {
+                string line = fileIn.ReadToEnd();
+                char[] sep = { ' ', '\n', '\t', '\r' };
+                string[] data = line.Split(sep);
+                foreach (string item in data)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        tree.Add(int.Parse(item));
+                    }
+                }
+            }
+            return tree;
+        }*/
+
+        static void Main(string[] args)
+        {
+            /* I. В входном файле указывается количество вершин графа/орграфа и матрица смежности:
+             * Для заданного орграфа:
+             * 13. добавить новую вершину.
+             */
+
+            //BinaryTree tree2 = ReadTree("input2.txt");
+        }
+    }
+}
